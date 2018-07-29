29b4e1e2e931910c5465f8157202cdc514710de9
huangwenwei <mr.huangwenwei@gmail.com>
2018-07-29 12:52:42 +0800

    Add command

---
+++ command.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+type configure struct {
+	ElasticURL   string
+	ElasticSniff bool
+}
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "bitcoin-service-external-api",
+	Short: "Bitcoin middleware for application",
+}
+
+// Execute 命令行入口
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+func init() {
+	config = new(configure)
+	config.InitConfig()
+}
+
+func (conf *configure) InitConfig() {
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(HomeDir())
+	viper.SetConfigName("bitcoin-service")
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	err := viper.ReadInConfig()
+	if err == nil {
+		fmt.Println("Using Configure file:", viper.ConfigFileUsed())
+	} else {
+		log.Fatal("Error: bitcoin.yml not found in: ", HomeDir())
+	}
+
+	for key, value := range viper.AllSettings() {
+		switch key {
+		case "elastic_url":
+			conf.ElasticURL = value.(string)
+		case "elastic_sniff":
+			conf.ElasticSniff = value.(bool)
+
+		}
+	}
+}
--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+var (
+	config *configure
+)
+
 func main() {
+	Execute()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
